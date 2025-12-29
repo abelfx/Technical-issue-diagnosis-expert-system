08695d7bb3597d64d3b9617fa32e8fa0166d166d
Abel Tesfa <abeltesfa@gmail.com>
2025-12-30 02:13:56 +0300

    feat: added solutions to issues

---
+++ domain/solutions.py
@@ -0,0 +1,5 @@
+SOLUTIONS = {
+    "power_supply_failure": "Check or replace the power supply.",
+    "display_issue": "Check display cables or graphics card.",
+    "cooling_problem": "Clean fans and replace thermal paste."
+}
